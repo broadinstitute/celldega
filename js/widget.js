1626ee332f75ffcad5af4c97095f4ae5d567c5ac
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-04-13 12:41:42 -0400

    setting up closure pattern

--- js/widget.js
+++ js/widget.js
@@ -23,21 +23,52 @@ export async function render({ model, el }) {
     // functions
     ////////////////
 
-	const grab_trx_tiles_in_view = async (tiles_in_view, options) => {
+    const grab_trx_tiles_in_view = async (tiles_in_view, options) => {
 
-		const tile_trx_urls = tiles_in_view.map(tile => {
-			return `${base_url}/real_transcript_tiles_mosaic/transcripts_tile_${tile.tileX}_${tile.tileY}.parquet`;
-		});
-	
-		var tile_trx_tables = await fetch_all_tables(cache_trx, tile_trx_urls, options)
-		var trx_arrow_table = concatenate_arrow_tables(tile_trx_tables)
-		trx_names_array = trx_arrow_table.getChild("name").toArray();
-		var trx_scatter_data = get_scatter_data(trx_arrow_table)
-	
-		return trx_scatter_data
-	}
+      const tile_trx_urls = tiles_in_view.map(tile => {
+        return `${base_url}/real_transcript_tiles_mosaic/transcripts_tile_${tile.tileX}_${tile.tileY}.parquet`;
+      });
+    
+      var tile_trx_tables = await fetch_all_tables(cache_trx, tile_trx_urls, options)
+      var trx_arrow_table = concatenate_arrow_tables(tile_trx_tables)
+      trx_names_array = trx_arrow_table.getChild("name").toArray();
+      var trx_scatter_data = get_scatter_data(trx_arrow_table)
+    
+      return trx_scatter_data
+    }
 
+    // const createGrabTrxTilesInView = (base_url, cache_trx, trx_names_array) => async (tiles_in_view, options) => {
+    //     const tile_trx_urls = tiles_in_view.map(tile => `${base_url}/real_transcript_tiles_mosaic/transcripts_tile_${tile.tileX}_${tile.tileY}.parquet`);
+    //     var tile_trx_tables = await fetch_all_tables(cache_trx, tile_trx_urls, options);
+    //     var trx_arrow_table = concatenate_arrow_tables(tile_trx_tables);
+    //     trx_names_array = trx_arrow_table.getChild("name").toArray();
+    //     var trx_scatter_data = get_scatter_data(trx_arrow_table);
+    //     return trx_scatter_data;
+    // };
 
+    // const create_grab_cell_tiles_in_view = (base_url, cache_trx, trx_names_array) => grab_trx_tiles_in_view
+
+    // // Factory function that returns a new function
+    // const create_real_fun = (test_variable) => {
+    //   return () => {
+    //       console.log('real_fun', test_variable);
+    //   };
+    // };
+
+    const def_real_fun = ( test_variable ) => () => {
+      console.log('real_fun', test_variable);
+    };    
+
+    // A test variable to be used in the function
+    const test_variable = 'something with closures!!!!! and skipping create_real_fun';
+
+    // Create a function using the factory
+    const real_fun = def_real_fun(test_variable);
+
+    // Execute the function created by the factory
+    real_fun();
+
+  
 
     const grab_cell_tiles_in_view = async (tiles_in_view) => {
 

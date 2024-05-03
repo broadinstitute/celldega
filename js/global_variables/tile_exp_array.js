4502552074d8a2aada0a6142c2a68fbbf61c2704
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-05-03 16:14:27 -0400

    cleaning tile data methods

--- js/global_variables/tile_exp_array.js
+++ js/global_variables/tile_exp_array.js
@@ -1,5 +1,10 @@
+import { get_new_tile_exp_array } from "../global_variables/tile_scatter_data.js";
+
 export let tile_exp_array 
 
-export const update_tile_exp_array = (new_tile_exp_array) => {
+export const update_tile_exp_array = async (base_url, inst_gene) => {
+
+    const new_tile_exp_array = await get_new_tile_exp_array(base_url, inst_gene)
+
     tile_exp_array = new_tile_exp_array
 }
--- js/viz/toy.js
+++ js/viz/toy.js
@@ -8,7 +8,7 @@ import { deck, set_deck } from '../deck-gl/toy_deck.js'
 import { update_layers } from "../deck-gl/toy_layers.js";
 import { square_scatter_layer, ini_square_scatter_layer } from "../deck-gl/square_scatter_layer.js";  
 
-import { get_new_tile_exp_array, update_tile_scatter_data } from "../global_variables/tile_scatter_data.js";
+import { update_tile_scatter_data } from "../global_variables/tile_scatter_data.js";
 import { update_tile_cats_array } from "../global_variables/tile_cats_array.js";
 import { update_tile_names_array } from "../global_variables/tile_names_array.js";
 import { update_tile_color_dict } from "../global_variables/tile_color_dict.js";
@@ -29,33 +29,9 @@ export const toy = async ( root, base_url ) => {
 
     await set_meta_gene(base_url)
 
+    // let new_tile_exp_array = await get_new_tile_exp_array(base_url, 'TSPAN3')
 
-    // let inst_gene = 'TSPAN3'
-    // var trx_table = await get_arrow_table(base_url + 'tbg/' + inst_gene + '.parquet', options.fetch)
-    // let trx_names = trx_table.getChild('__index_level_0__').toArray()
-    // let trx_exp = trx_table.getChild(inst_gene).toArray()    
-    
-    // const name_to_index_map = new Map();
-    // tile_names_array.forEach((name, index) => {
-    //     name_to_index_map.set(name, index);
-    // });
-
-    // let trx_exp = get_new_tile_exp_array('TSPAN3')
-
-    // const new_tile_exp_array = new Array(tile_cats_array.length).fill(0);
-
-    // trx_names.forEach((name, i) => {
-    //     if (name_to_index_map.has(name)) {
-    //         const index = name_to_index_map.get(name);
-    //         const exp_value = Number(trx_exp[i]);
-    //         const max_exp = Number(meta_gene[inst_gene].max); 
-    //         new_tile_exp_array[index] = (exp_value / max_exp) * 255;
-    //     }
-    // });
-
-    let new_tile_exp_array = await get_new_tile_exp_array(base_url, 'TSPAN3')
-
-    update_tile_exp_array(new_tile_exp_array)
+    await update_tile_exp_array(base_url, 'TSPAN3')
     
     await update_tile_color_dict(base_url)
 

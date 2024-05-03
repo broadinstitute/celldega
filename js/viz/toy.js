24d22bd53e7f4eca228b8765fcbc91dce9b2fba1
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-05-03 16:08:06 -0400

    made get_new_tile_exp_array into a method

--- js/viz/toy.js
+++ js/viz/toy.js
@@ -8,12 +8,12 @@ import { deck, set_deck } from '../deck-gl/toy_deck.js'
 import { update_layers } from "../deck-gl/toy_layers.js";
 import { square_scatter_layer, ini_square_scatter_layer } from "../deck-gl/square_scatter_layer.js";  
 
-import { update_tile_scatter_data } from "../global_variables/tile_scatter_data.js";
-import { tile_cats_array, update_tile_cats_array } from "../global_variables/tile_cats_array.js";
-import { tile_names_array, update_tile_names_array } from "../global_variables/tile_names_array.js";
+import { get_new_tile_exp_array, update_tile_scatter_data } from "../global_variables/tile_scatter_data.js";
+import { update_tile_cats_array } from "../global_variables/tile_cats_array.js";
+import { update_tile_names_array } from "../global_variables/tile_names_array.js";
 import { update_tile_color_dict } from "../global_variables/tile_color_dict.js";
 import { update_tile_exp_array } from "../global_variables/tile_exp_array.js"; 
-import { meta_gene, set_meta_gene } from "../global_variables/meta_gene.js";
+import { set_meta_gene } from "../global_variables/meta_gene.js";
 
 export const toy = async ( root, base_url ) => {
 
@@ -23,35 +23,37 @@ export const toy = async ( root, base_url ) => {
 
     var tile_arrow_table = await get_arrow_table(tile_url, options.fetch)
 
-
     update_tile_scatter_data(get_scatter_data(tile_arrow_table))
     update_tile_cats_array(tile_arrow_table.getChild("cluster").toArray())
     update_tile_names_array(tile_arrow_table.getChild("name").toArray())
 
-
     await set_meta_gene(base_url)
 
 
-    let inst_gene = 'TSPAN3'
-    var trx_table = await get_arrow_table(base_url + 'tbg/' + inst_gene + '.parquet', options.fetch)
-    let trx_names = trx_table.getChild('__index_level_0__').toArray()
-    let trx_exp = trx_table.getChild(inst_gene).toArray()    
+    // let inst_gene = 'TSPAN3'
+    // var trx_table = await get_arrow_table(base_url + 'tbg/' + inst_gene + '.parquet', options.fetch)
+    // let trx_names = trx_table.getChild('__index_level_0__').toArray()
+    // let trx_exp = trx_table.getChild(inst_gene).toArray()    
     
-    const name_to_index_map = new Map();
-    tile_names_array.forEach((name, index) => {
-        name_to_index_map.set(name, index);
-    });
+    // const name_to_index_map = new Map();
+    // tile_names_array.forEach((name, index) => {
+    //     name_to_index_map.set(name, index);
+    // });
 
-    const new_tile_exp_array = new Array(tile_cats_array.length).fill(0);
+    // let trx_exp = get_new_tile_exp_array('TSPAN3')
 
-    trx_names.forEach((name, i) => {
-        if (name_to_index_map.has(name)) {
-            const index = name_to_index_map.get(name);
-            const exp_value = Number(trx_exp[i]);
-            const max_exp = Number(meta_gene[inst_gene].max); // Ensure this is also a Number
-            new_tile_exp_array[index] = (exp_value / max_exp) * 255;
-        }
-    });
+    // const new_tile_exp_array = new Array(tile_cats_array.length).fill(0);
+
+    // trx_names.forEach((name, i) => {
+    //     if (name_to_index_map.has(name)) {
+    //         const index = name_to_index_map.get(name);
+    //         const exp_value = Number(trx_exp[i]);
+    //         const max_exp = Number(meta_gene[inst_gene].max); 
+    //         new_tile_exp_array[index] = (exp_value / max_exp) * 255;
+    //     }
+    // });
+
+    let new_tile_exp_array = await get_new_tile_exp_array(base_url, 'TSPAN3')
 
     update_tile_exp_array(new_tile_exp_array)
     

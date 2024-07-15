80088ed20b6e127541cbe00946a20668267ae5eb
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-07-15 12:58:55 -0400

    have option for black to red color scheme

--- js/deck-gl/cell_layer.js
+++ js/deck-gl/cell_layer.js
@@ -8,6 +8,7 @@ import { options } from '../global_variables/fetch_options.js'
 import { cat } from '../global_variables/cat.js'
 import { cell_exp_array } from '../global_variables/cell_exp_array.js'
 
+// // transparent to red
 // const cell_layer_color = (i, d) => {
 //     if (cat === 'cluster') {
 //         // const inst_cat = tile_cats_array[d.index];
@@ -24,20 +25,89 @@ import { cell_exp_array } from '../global_variables/cell_exp_array.js'
 //     }
 // }
 
+// // opaque white to red
+// /////////////////////////////////////
+// const cell_layer_color = (i, d) => {
+//     if (cat === 'cluster') {
+//         return [0, 0, 255, 255]; // Use blue for cluster category
+//     } else {
+//         const inst_exp = cell_exp_array[d.index];
+
+//         // Interpolate between white and red based on the expression value
+//         const red = Math.floor((inst_exp / 255) * 255);
+//         const green = Math.floor((inst_exp / 255) * 255); // To blend from white to red, green and blue components also change
+//         const blue = Math.floor((inst_exp / 255) * 255);  // To blend from white to red, green and blue components also change
+
+//         return [255, 255 - green, 255 - blue, 255]; // Full opacity
+//     }
+// };
+
+// Function to generate cell layer color
 const cell_layer_color = (i, d) => {
+
+    const color_scheme = 'black_to_red'
     if (cat === 'cluster') {
         return [0, 0, 255, 255]; // Use blue for cluster category
     } else {
         const inst_exp = cell_exp_array[d.index];
-
-        // Interpolate between white and red based on the expression value
         const red = Math.floor((inst_exp / 255) * 255);
-        const green = Math.floor((inst_exp / 255) * 255); // To blend from white to red, green and blue components also change
-        const blue = Math.floor((inst_exp / 255) * 255);  // To blend from white to red, green and blue components also change
+        let color;
 
-        return [255, 255 - green, 255 - blue, 255]; // Full opacity
+        if (color_scheme === 'white_to_red') {
+            const green = 255 - red; // Green decreases as red increases
+            const blue = 255 - red; // Blue decreases as red increases
+            color = [255, green, blue, 255]; // Full opacity
+        } else if (color_scheme === 'black_to_red') {
+            color = [red, 0, 0, 255]; // From black to red with full opacity
+        }
+
+        return color;
     }
 };
+
+// // Example usage:
+// const color_white_to_red = cell_layer_color(i, d, 'white_to_red');
+// const color_black_to_red = cell_layer_color(i, d, 'black_to_red');
+
+
+// // blue to white to red
+// const cell_layer_color = (i, d) => {
+//     if (cat === 'cluster') {
+//         return [0, 0, 255, 255]; // Use blue for cluster category
+//     } else {
+//         const inst_exp = cell_exp_array[d.index];
+
+//         // Define the three colors
+//         const lowColor = [0, 0, 255]; // Blue
+//         const midColor = [255, 255, 255]; // White
+//         const highColor = [255, 0, 0]; // Red
+
+//         // Determine the interpolation factor (between 0 and 1)
+//         const factor = inst_exp / 255;
+
+//         let color;
+
+//         if (factor < 0.5) {
+//             // Interpolate between lowColor and midColor
+//             const interpFactor = factor * 2;
+//             color = [
+//                 Math.floor(lowColor[0] + interpFactor * (midColor[0] - lowColor[0])),
+//                 Math.floor(lowColor[1] + interpFactor * (midColor[1] - lowColor[1])),
+//                 Math.floor(lowColor[2] + interpFactor * (midColor[2] - lowColor[2]))
+//             ];
+//         } else {
+//             // Interpolate between midColor and highColor
+//             const interpFactor = (factor - 0.5) * 2;
+//             color = [
+//                 Math.floor(midColor[0] + interpFactor * (highColor[0] - midColor[0])),
+//                 Math.floor(midColor[1] + interpFactor * (highColor[1] - midColor[1])),
+//                 Math.floor(midColor[2] + interpFactor * (highColor[2] - midColor[2]))
+//             ];
+//         }
+
+//         return [...color, 255]; // Full opacity
+//     }
+// };
 
 
 export let cell_layer = new ScatterplotLayer({

7716d2870835868e34f9d506c54c1fc0e38b48bb
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-10-30 10:11:06 -0400

    heatmap is setting gene

--- js/widget_interactions/update_tile_landscape_from_cgm.js
+++ js/widget_interactions/update_tile_landscape_from_cgm.js
@@ -1,12 +1,13 @@
 // import { model } from '../global_variables/model.js'
-// // import { update_cat } from "../global_variables/cat.js"
-// import { update_tile_exp_array } from "../global_variables/tile_exp_array.js";
+import { update_cat } from "../global_variables/cat.js"
+import { update_tile_exp_array } from "../global_variables/tile_exp_array.js";
 // import { global_base_url } from '../global_variables/global_base_url.js';
 // // import { update_selected_cats } from "../global_variables/cat";
-// import { gene_search_input } from "../ui/gene_search_input.js";
-// import { square_scatter_layer, update_square_scatter_layer } from "../deck-gl/square_scatter_layer.js";
+// import { set_gene_search_input } from "../ui/gene_search_input.js";
+import { update_square_scatter_layer } from "../deck-gl/square_scatter_layer.js";
 // import { deck_sst } from "../deck-gl/deck_sst.js";
 // import { simple_image_layer } from "../deck-gl/simple_image_layer.js";
+import { update_gene_text_box } from "../ui/gene_search.js";
 
 export const update_tile_landscape_from_cgm = async (deck_sst, layers_sst, viz_state) => {
 
@@ -14,35 +15,41 @@ export const update_tile_landscape_from_cgm = async (deck_sst, layers_sst, viz_s
 
     const click_info = viz_state.model.get('update_trigger');
 
-    console.log('click_info', click_info)
+    let inst_gene
 
-    // let selected_gene
-    // if (click_info.click_type === 'row-label') {
+    if (click_info.click_type === 'row-label') {
 
-    //     selected_gene = click_info.click_value
-    //     update_cat(selected_gene)
-    //     await update_tile_exp_array(global_base_url, selected_gene)
+        inst_gene = click_info.click_value
+        update_cat(viz_state.cats, inst_gene)
+        await update_tile_exp_array(viz_state, inst_gene)
+        //
 
-    // } else if (click_info.click_type === 'col-label') {
+    } else if (click_info.click_type === 'col-label') {
 
-    //     selected_gene = 'cluster'
-    //     update_cat(selected_gene)
-    //     // update_selected_cats(viz_state.cats, [click_info.click_value])
+        // inst_gene = 'cluster'
+        // update_cat(inst_gene)
+        // // update_selected_cats(viz_state.cats, [click_info.click_value])
 
-    // } else if (click_info.click_type === 'col-dendro') {
+    } else if (click_info.click_type === 'col-dendro') {
 
-    //     selected_gene = 'cluster'
-    //     update_cat(selected_gene)
-    //     // update_selected_cats(viz_state.cats, click_info.click_value)
+        // inst_gene = 'cluster'
+        // update_cat(inst_gene)
+        // // update_selected_cats(viz_state.cats, click_info.click_value)
 
-    // } else {
-    //     selected_gene = 'cluster'
-    //     update_cat(selected_gene)
-    // }
+    } else {
 
-    // gene_search_input.value = selected_gene
+        // inst_gene = 'cluster'
+        // update_cat(inst_gene)
 
-    // update_square_scatter_layer()
-    // deck_sst.setProps({layers: [simple_image_layer, square_scatter_layer]})
+    }
+
+    // gene_search_input.value = inst_gene
+
+    update_square_scatter_layer(viz_state, layers_sst)
+    deck_sst.setProps({layers: [layers_sst.simple_image_layer, layers_sst.square_scatter_layer]})
+
+    viz_state.genes.gene_search_input.value = (viz_state.genes.gene_search_input.value !== inst_gene) ? inst_gene : ''
+
+    await update_gene_text_box(viz_state.genes, inst_gene)
 
 }

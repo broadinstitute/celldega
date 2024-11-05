b253daa01f99fb9244bf0332a4feab7ef570e723
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-11-05 11:58:06 -0500

    trying to simplify vertex shader

--- js/viz/matrix_viz.js
+++ js/viz/matrix_viz.js
@@ -1009,7 +1009,6 @@ export const matrix_viz = async (
             out float innerUnitRadius;
             out float outerRadiusPixels;
 
-
             void main(void) {
             geometry.worldPosition = instancePositions;
 
@@ -1019,37 +1018,24 @@ export const matrix_viz = async (
                 radiusMinPixels, radiusMaxPixels
             );
 
-            // Multiply out line width and clamp to limits
-            float lineWidthPixels = clamp(
-                project_size_to_pixel(lineWidthScale * instanceLineWidths, lineWidthUnits),
-                lineWidthMinPixels, lineWidthMaxPixels
-            );
-
-            // outer radius needs to offset by half stroke width
-            outerRadiusPixels += stroked * lineWidthPixels / 2.0;
+            // // fixed size scatterplot
+            // outerRadiusPixels = 10.0;
 
             // Expand geometry to accomodate edge smoothing
-            float edgePadding = antialiasing ? (outerRadiusPixels + SMOOTH_EDGE_RADIUS) / outerRadiusPixels : 1.0;
+            float edgePadding = 1.0; // antialiasing ? (outerRadiusPixels + SMOOTH_EDGE_RADIUS) / outerRadiusPixels : 1.0;
 
             // position on the containing square in [-1, 1] space
             unitPosition = edgePadding * positions.xy;
             geometry.uv = unitPosition;
             geometry.pickingColor = instancePickingColors;
 
-            innerUnitRadius = 1.0 - stroked * lineWidthPixels / outerRadiusPixels;
 
-            if (billboard) {
-                gl_Position = project_position_to_clipspace(instancePositions, instancePositions64Low, vec3(0.0), geometry.position);
-                DECKGL_FILTER_GL_POSITION(gl_Position, geometry);
-                vec3 offset = edgePadding * positions * outerRadiusPixels;
-                DECKGL_FILTER_SIZE(offset, geometry);
-                gl_Position.xy += project_pixel_size_to_clipspace(offset.xy);
-            } else {
-                vec3 offset = edgePadding * positions * project_pixel_size(outerRadiusPixels);
-                DECKGL_FILTER_SIZE(offset, geometry);
-                gl_Position = project_position_to_clipspace(instancePositions, instancePositions64Low, offset, geometry.position);
-                DECKGL_FILTER_GL_POSITION(gl_Position, geometry);
-            }
+            innerUnitRadius = 0.0; //  1.0 - stroked * lineWidthPixels / outerRadiusPixels;
+
+            vec3 offset = edgePadding * positions * project_pixel_size(outerRadiusPixels);
+            // DECKGL_FILTER_SIZE(offset, geometry);
+            gl_Position = project_position_to_clipspace(instancePositions, instancePositions64Low, offset, geometry.position);
+            // DECKGL_FILTER_GL_POSITION(gl_Position, geometry);
 
             // Apply opacity to instance color, or return instance picking color
             vFillColor = vec4(instanceFillColors.rgb, instanceFillColors.a * opacity);

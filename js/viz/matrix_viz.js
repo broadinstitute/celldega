c81aab408f98e956c157eff9042b0a0b28c6d2e2
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-11-16 13:13:47 -0500

    added license information from the original Clustergrammer-gl library

--- js/viz/matrix_viz.js
+++ js/viz/matrix_viz.js
@@ -1,3 +1,30 @@
+// The Celldega Matrix Vizualization Method is being built using the approaches
+// and code adaptations from the Clustergrammer-GL library, which is available at
+// github.com/ismms-himc/clustergrammer-gl
+// and being used under the license
+//
+// MIT License
+
+// Copyright (c) 2021 Nicolas Fernandez
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import { ini_deck } from '../deck-gl/matrix/deck_mat.js'
 import { set_mat_data } from '../matrix/mat_data.js';
 import { set_mat_constants } from '../matrix/set_constants.js';

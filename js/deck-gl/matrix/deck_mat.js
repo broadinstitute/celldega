bc253a42d066a3b954a9f3b98e1e0964be699b74
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-11-20 13:01:11 -0500

    removed console logs

--- js/deck-gl/matrix/deck_mat.js
+++ js/deck-gl/matrix/deck_mat.js
@@ -9,16 +9,12 @@ const getCursor = ({ isDragging }) => {
 
 export const ini_deck = ( root, width, height ) => {
 
-    console.log('width:', width)
-
     // if width is a number then it is implied to be in pixels
     // and needs to have a buffer added to it
     if (typeof width === 'number'){
         width = width + 100
         height = height + 100
     }
-
-    console.log('width:', width)
 
     let deck_ist = new Deck({
         parent: root,

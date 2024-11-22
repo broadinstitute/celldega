172a1faa76d220fadc0f6e6328cc23d98b916830
Jaspreet Ishar <jaspreetishar2000@gmail.com>
2024-11-22 10:22:21 -0500

    xenium-default support, remove cells per um2 metric, concise metric names

--- src/celldega/qc/__init__.py
+++ src/celldega/qc/__init__.py
@@ -3,16 +3,17 @@ import numpy as np
 import os
 import geopandas as gpd
 import tifffile as tiff
+import alphashape
 
-def processing(transcript_metadata_file, transcript_data_file, cell_polygon_metadata_file, cell_polygon_data_file, thickness, subset_interval_y_x, pixel_size, tech_name):
+def qc_segmentation(transcript_metadata_file, transcript_data_file, cell_polygon_metadata_file, cell_polygon_data_file, subset_interval_y_x, pixel_size, dataset_name, segmentation_approach, from_stp):
 
     """
     A function to calculate segmentation quality control
     metrics for imaging spatial transcriptomics data.
     """
 
-    metrics = {}    
-    trx_meta = pd.read_parquet(transcript_metadata_file)  
+    metrics = {}
+    trx_meta = pd.read_parquet(transcript_metadata_file)
 
     if transcript_data_file.endswith(".csv"):
         trx = pd.read_csv(transcript_data_file)
@@ -20,46 +21,48 @@ def processing(transcript_metadata_file, transcript_data_file, cell_polygon_meta
         trx = gpd.read_parquet(transcript_data_file)
     else:
         raise ValueError("Invalid file type. A .csv or .parquet file must be provided.")
+    
+    if from_stp:
+        cell_index = "cell_index"
+        gene = "gene"
+        transcript_index = "transcript_index"
+    else:
+        cell_index = "cell_id"
+        gene = "feature_name"
+        transcript_index = "transcript_id"
 
     cell_gdf = gpd.read_parquet(cell_polygon_data_file)
     cell_meta_gdf = gpd.read_parquet(cell_polygon_metadata_file)
     
-    percentage_of_assigned_transcripts = (len(trx_meta) / len(trx)) * 100
+    percentage_of_assigned_transcripts = (len(trx_meta) / len(trx))
+
+    metrics['dataset_name'] = dataset_name
+    metrics['segmentation_approach'] = segmentation_approach
     
-    metrics['proportion_transcripts_assigned_to_cells'] = percentage_of_assigned_transcripts
-    metrics['total_number_of_cells'] = len(cell_gdf)
-    metrics['average_cell_area'] = cell_gdf['geometry'].area.mean()
-    metrics['average_cell_volume'] = (cell_gdf['geometry'].area * thickness).mean()
+    metrics['proportion_assigned_transcripts'] = percentage_of_assigned_transcripts
+    metrics['number_cells'] = len(cell_gdf)
+    metrics['mean_cell_area'] = cell_gdf['geometry'].area.mean()
     
-    metrics['average_transcripts_per_cell'] = trx_meta.groupby('cell_index').size().mean()
-    metrics['median_transcripts_per_cell'] = trx_meta.groupby("cell_index")["transcript_index"].count().median()
+    metrics['mean_transcripts_per_cell'] = trx_meta.groupby(cell_index).size().mean()
+    metrics['median_transcripts_per_cell'] = trx_meta.groupby(cell_index)[transcript_index].count().median()
 
-    metrics['average_genes_per_cell'] = trx_meta.groupby('cell_index')['gene'].nunique().mean()
-    metrics['median_genes_per_cell'] = trx_meta.groupby("cell_index")["gene"].nunique().median()
+    metrics['average_genes_per_cell'] = trx_meta.groupby(cell_index)[gene].nunique().mean()
+    metrics['median_genes_per_cell'] = trx_meta.groupby(cell_index)[gene].nunique().median()
 
-    width_um = subset_interval_y_x[3] * pixel_size
-    height_um = subset_interval_y_x[1] * pixel_size
-    total_area_um2 = width_um * height_um
-    num_units = total_area_um2 / 100
-    polygons_per_unit = len(cell_gdf) / num_units
-
-    metrics['cells_per_100_um^2'] = polygons_per_unit
-
-    metrics['percent_empty_cells'] = ((len(cell_meta_gdf) - len(cell_gdf)) / len(cell_meta_gdf)) * 100
+    metrics['proportion_empty_cells'] = ((len(cell_meta_gdf) - len(cell_gdf)) / len(cell_meta_gdf))
 
     metrics_df = pd.DataFrame([metrics])
     metrics_df = metrics_df.T
-    metrics_df.columns = [tech_name]
+    metrics_df.columns = [f"{dataset_name}-{segmentation_approach}"]
     metrics_df = metrics_df.T
     
     gene_specific_metrics_df = pd.DataFrame({
-        "proportion_of_cells_expressing_gene": (trx_meta.groupby('gene')['cell_index'].nunique()) / len(cell_gdf),
-        "average_expression_of_gene": trx_meta.groupby('gene')['cell_index'].mean(),
-        "assigned_transcripts_per_gene": (trx_meta.groupby("gene")["transcript_index"].count() / trx.groupby("feature_name")["transcript_id"].count()).fillna(0)
-    }).T
+        "proportion_of_cells_expressing": (trx_meta.groupby(gene)[cell_index].nunique()) / len(cell_gdf),
+        "average_expression": trx_meta.groupby(gene)[cell_index].mean(),
+        "assigned_transcripts": (trx_meta.groupby(gene)[transcript_index].count() / trx.groupby("feature_name")["transcript_id"].count()).fillna(0)
+    })
 
-    gene_specific_metrics_df.index.name = "metric_name"
+    metrics_df.to_csv(f"qc_segmentation_{dataset_name}-{segmentation_approach}.csv")
+    gene_specific_metrics_df.to_parquet(f"gene_specific_qc_segmentation_{dataset_name}-{segmentation_approach}.parquet")
 
     print("segmentation metrics calculation completed")
-
-    return metrics_df, gene_specific_metrics_df

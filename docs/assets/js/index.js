f32342dca4fb9e7aaf3fa8bfce834489a2d5ff07
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-12-06 19:36:59 -0500

    loading celldega.js

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,14 +1,25 @@
 // import celldega from '.assets/js/widget.js'; // Adjust path as needed
 // import celldega from 'https://unpkg.com/celldega@0.5.0/src/celldega/static/widget.js?module';
+
+// import celldega from './assets/js/widget.js';
+import celldega from './widget.js';
+
 document.addEventListener("DOMContentLoaded", () => {
 
     console.log("hello hello");
 
     // console.log(celldega)
 
-    // import celldega from './assets/js/widget.js';
 
-    // console.log('celldega', celldega)
+
+    console.log('celldega', celldega)
+
+    console.log('*****************************')
+    console.log('*****************************')
+    console.log('*****************************')
+    console.log('*****************************')
+    console.log('*****************************')
+    console.log('*****************************')
 
 
 });

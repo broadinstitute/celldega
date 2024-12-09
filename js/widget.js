1c5ca31288a4a3e6fcfe1b36f9595c0e2d6f460f
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2024-12-09 10:45:50 -0500

    cleaning

--- js/widget.js
+++ js/widget.js
@@ -1,22 +1,7 @@
-/* eslint-disable no-unused-vars */
-
 import "./widget.css";
 import { landscape_ist } from "./viz/landscape_ist";
 import { landscape_sst } from "./viz/landscape_sst";
 import { matrix_viz } from "./viz/matrix_viz";
-// import cgm, { type } from 'clustergrammer-gl';
-// import _ from 'underscore';
-
-// // Ensure these variables are defined globally
-// const globalVariables =
-//   ['mat_data', 'manual_category', 'control_svg', 'run_cluster_container',
-//   'link_options_container', 'selected_label_container', 'network',
-//   'max_clust_id', 'new_clust_id', 'ini_value_color', '_', 'organism',
-//   'num_matches'];
-
-// globalVariables.forEach((variableName) => {
-//     window[variableName] = window[variableName] || {};
-// });
 
 export const render_landscape = async ({ model, el }) => {
 
@@ -102,68 +87,6 @@ export const render_matrix_new = async ({ model, el }) => {
     matrix_viz(model, el, network, width, height)
 
 }
-
-// export const render_matrix = async ({ model, el }) => {
-
-//     var my_widget_callback = function(external_model){
-
-//         const click_type = this.params.tooltip.tooltip_type
-//         var click_value = null
-
-//         if (click_type === 'row-label'){
-//             click_value = this.params.int.mouseover.row.name
-//         } else if (click_type === 'col-label'){
-//             click_value = this.params.int.mouseover.col.name
-//         } else if (click_type === 'col-dendro'){
-//             click_value = this.params.dendro.selected_clust_names
-//         } else if (click_type === 'row-dendro'){
-//             click_value = this.params.dendro.selected_clust_names
-//         }
-
-//         var click_info = {
-//             'click_type': click_type,
-//             'click_value': click_value
-//         }
-
-//         // Update the click_info trait in the model with the new value
-//         model.set('click_info', click_info);
-//         model.save_changes();
-
-//     }
-
-//     var network = model.get('network')
-//     let container = document.createElement("div")
-//     container.style.height = "1000px";
-//     container.id = "cgm-container";
-//     el.appendChild(container)
-
-//     setTimeout(function() {
-
-//         cgm({
-//             network: network,
-//             viz_width: 900,
-//             viz_height: 900,
-//             widget_callback: my_widget_callback,
-//             is_widget: true,
-//             container: container,
-//             use_hzome: true
-//         },
-//         'yes this is a widget'
-//         );
-
-//     }, 1000)
-
-
-//     // trying to remove old tooltips
-//     // Select all elements with the class 'cgm-tooltip'
-//     var elements = document.querySelectorAll('.cgm-tooltip');
-
-//     // Loop through the NodeList and remove each element
-//     elements.forEach(function(element) {
-//         element.parentNode.removeChild(element);
-//     });
-
-// }
 
 export const render = async ({ model, el }) => {
 

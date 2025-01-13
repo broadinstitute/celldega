7821d7bfbbd917057fa7cc6be6f0726979b3fd6c
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-01-13 16:38:16 -0500

    front-end is auto-scaling umap coordinates

--- js/deck-gl/cell_layer.js
+++ js/deck-gl/cell_layer.js
@@ -211,45 +211,36 @@ export const ini_cell_layer = async (base_url, viz_state) => {
     const y_min = d3.min(cell_scatter_data_objects.map(d => d.position[1]))
     const y_max = d3.max(cell_scatter_data_objects.map(d => d.position[1]))
 
-    const umap_x_min = d3.min(cell_scatter_data_objects.map(d => d.umap[0]))
-    const umap_x_max = d3.max(cell_scatter_data_objects.map(d => d.umap[0]))
-    const umap_y_min = d3.min(cell_scatter_data_objects.map(d => d.umap[1]))
-    const umap_y_max = d3.max(cell_scatter_data_objects.map(d => d.umap[1]))
+    // take the smaller of the two ranges for x and y
+    const x_range = x_max - x_min
+    const y_range = y_max - y_min
+
+    const range_min = Math.min(x_range, y_range)
 
     const x_mid = (x_max - x_min) / 2;
     const y_mid = (y_max - y_min) / 2;
 
+    let umap_x_min = d3.min(cell_scatter_data_objects.map(d => d.umap[0]))
+    let umap_x_max = d3.max(cell_scatter_data_objects.map(d => d.umap[0]))
+    let umap_y_min = d3.min(cell_scatter_data_objects.map(d => d.umap[1]))
+    let umap_y_max = d3.max(cell_scatter_data_objects.map(d => d.umap[1]))
+
+    const umap_x_range = umap_x_max - umap_x_min
+    const umap_y_range = umap_y_max - umap_y_min
+
+    // scale the umap values to be within range_min and c   entered about x_mid and y_mid
+    cell_scatter_data_objects.forEach(d => {
+        d.umap[0] = (d.umap[0] - umap_x_min) / (umap_x_range) * range_min
+        d.umap[1] = (d.umap[1] - umap_y_min) / (umap_y_range) * range_min
+    })
+
+    umap_x_min = d3.min(cell_scatter_data_objects.map(d => d.umap[0]))
+    umap_x_max = d3.max(cell_scatter_data_objects.map(d => d.umap[0]))
+    umap_y_min = d3.min(cell_scatter_data_objects.map(d => d.umap[1]))
+    umap_y_max = d3.max(cell_scatter_data_objects.map(d => d.umap[1]))
+
     const umap_x_mid = (umap_x_max - umap_x_min) / 2;
     const umap_y_mid = (umap_y_max - umap_y_min) / 2;
-
-
-    // const umap_x = d3.mean(cell_scatter_data_objects.map(d => d.umap[0]))
-    // const umap_y = d3.mean(cell_scatter_data_objects.map(d => d.umap[1]))
-
-    // const x_diff = mean_x - umap_x
-    // const y_diff = mean_y - umap_y
-
-    // cell_scatter_data_objects.forEach(d => {
-    //     d.umap[0] += x_diff
-    //     d.umap[1] += y_diff
-    // })
-
-
-    // const x_min = Math.min(...flatCoordinateArray.filter((_, i) => i % 2 === 0));
-    // const x_max = Math.max(...flatCoordinateArray.filter((_, i) => i % 2 === 0));
-    // const y_min = Math.min(...flatCoordinateArray.filter((_, i) => i % 2 === 1));
-    // const y_max = Math.max(...flatCoordinateArray.filter((_, i) => i % 2 === 1));
-
-    // const x_mid = (x_max - x_min) / 2;
-    // const y_mid = (y_max - y_min) / 2;
-
-    // const x_umap_min = Math.min(...flatCoordinateArray_umap.filter((_, i) => i % 2 === 0));
-    // const x_umap_max = Math.max(...flatCoordinateArray_umap.filter((_, i) => i % 2 === 0));
-    // const y_umap_min = Math.min(...flatCoordinateArray_umap.filter((_, i) => i % 2 === 1));
-    // const y_umap_max = Math.max(...flatCoordinateArray_umap.filter((_, i) => i % 2 === 1));
-
-    // const x_umap_mid = (x_umap_max - x_umap_min) / 2;
-    // const y_umap_mid = (y_umap_max - y_umap_min) / 2;
 
     const x_diff = x_mid - umap_x_mid;
     const y_diff = y_mid - umap_y_mid;

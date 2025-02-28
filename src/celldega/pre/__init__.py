cdf8a95f0a1167bca9f948050e447342a505cd08
Huan Wang <huanlity@gmail.com>
2025-02-28 08:26:56 -0500

    google style docstring

--- src/celldega/pre/__init__.py
+++ src/celldega/pre/__init__.py
@@ -24,12 +24,13 @@ from .landscape import *
 from .trx_tile import *
 from .boundary_tile import *
 
+
 def convert_long_id_to_short(df):
-    """
-    Converts a column of long integer cell IDs in a DataFrame to a shorter, hash-based representation.
+    """Converts a column of long integer cell IDs in a DataFrame to a shorter, hash-based representation.
 
     Args:
-        df (pd.DataFrame): The DataFrame containing the EntityID.
+        df (pd.DataFrame): The DataFrame containing the `EntityID` column.
+
     Returns:
         pd.DataFrame: The original DataFrame with an additional column named `cell_id`
                       containing the shortened cell IDs.
@@ -37,7 +38,6 @@ def convert_long_id_to_short(df):
     The function applies a SHA-256 hash to each cell ID, encodes the hash using base64, and truncates
     it to create a shorter identifier that is added as a new column to the DataFrame.
     """
-    # Function to hash and encode the cell ID
     def hash_and_shorten_id(cell_id):
         # Create a hash of the cell ID
         cell_id_bytes = str(cell_id).encode('utf-8')
@@ -55,23 +55,17 @@ def convert_long_id_to_short(df):
 
 
 def reduce_image_size(image_path, scale_image=0.5, path_landscape_files=""):
+    """Reduces the size of an image by a specified scale factor.
+
+    Args:
+        image_path (str): Path to the image file.
+        scale_image (float, optional): Scale factor for the image resize. Defaults to 0.5.
+        path_landscape_files (str, optional): Directory to save the resized image. Defaults to "".
+
+    Returns:
+        str: Path to the resized image file.
     """
-
-    Parameters
-    ----------
-    image_path : str
-        Path to the image file
-    scale_image : float (default=0.5)
-        Scale factor for the image resize
-
-    Returns
-    -------
-    new_image_path : str
-        Path to the resized image file
-    """
-
     image = pyvips.Image.new_from_file(image_path, access="sequential")
-
     resized_image = image.resize(scale_image)
 
     new_image_name = image_path.split("/")[-1].replace(".tif", "_downsize.tif")
@@ -82,172 +76,97 @@ def reduce_image_size(image_path, scale_image=0.5, path_landscape_files=""):
 
 
 def convert_to_jpeg(image_path, quality=80):
+    """Converts a TIFF image to a JPEG image with a specified quality score.
+
+    Args:
+        image_path (str): Path to the image file.
+        quality (int, optional): Quality score for the JPEG image. Defaults to 80.
+
+    Returns:
+        str: Path to the JPEG image file.
     """
-    Convert a TIFF image to a JPEG image with a quality of score
-
-    Parameters
-    ----------
-    image_path : str
-        Path to the image file
-    quality : int (default=80)
-        Quality score for the JPEG image
-
-    Returns
-    -------
-    new_image_path : str
-        Path to the JPEG image file
-
-    """
-
-    # Load the TIFF image
     image = pyvips.Image.new_from_file(image_path, access="sequential")
-
-    # Save the image as a JPEG with a quality of 80
     new_image_path = image_path.replace(".tif", ".jpeg")
     image.jpegsave(new_image_path, Q=quality)
 
     return new_image_path
 
+
 def convert_to_png(image_path):
+    """Converts a TIFF image to a PNG image.
+
+    Args:
+        image_path (str): Path to the image file.
+
+    Returns:
+        str: Path to the PNG image file.
     """
-    Convert a TIFF image to a JPEG image with a quality of score
-
-    Parameters
-    ----------
-    image_path : str
-        Path to the image file
-    quality : int (default=80)
-        Quality score for the JPEG image
-
-    Returns
-    -------
-    new_image_path : str
-        Path to the JPEG image file
-
-    """
-
-    # Load the TIFF image
     image = pyvips.Image.new_from_file(image_path, access="sequential")
-
-    # Save the image as a JPEG with a quality of 80
     new_image_path = image_path.replace(".tif", ".png")
     image.pngsave(new_image_path)
 
     return new_image_path
 
 
-
 def convert_to_webp(image_path, quality=100):
-    """
-    Convert a TIFF image to a WEBP image with a specified quality score.
+    """Converts a TIFF image to a WEBP image with a specified quality score.
 
-    Parameters
-    ----------
-    image_path : str
-        Path to the image file
-    quality : int (default=100)
-        Quality score for the WEBP image (higher is better quality)
+    Args:
+        image_path (str): Path to the image file.
+        quality (int, optional): Quality score for the WEBP image. Defaults to 100.
 
-    Returns
-    -------
-    new_image_path : str
-        Path to the WEBP image file
+    Returns:
+        str: Path to the WEBP image file.
     """
-    # Load the TIFF image
     image = pyvips.Image.new_from_file(image_path, access="sequential")
-
-    # Save the image as a WEBP with specified quality
     new_image_path = image_path.replace(".tif", ".webp")
     image.webpsave(new_image_path, Q=quality)
 
     return new_image_path
 
 
+def make_deepzoom_pyramid(image_path, output_path, pyramid_name, tile_size=512, overlap=0, suffix=".jpeg"):
+    """Creates a DeepZoom image pyramid from a JPEG image.
 
-def make_deepzoom_pyramid(
-    image_path, output_path, pyramid_name, tile_size=512, overlap=0, suffix=".jpeg"
-):
+    Args:
+        image_path (str): Path to the JPEG image file.
+        output_path (str): Directory to save the DeepZoom pyramid.
+        pyramid_name (str): Name of the pyramid directory.
+        tile_size (int, optional): Tile size for the DeepZoom pyramid. Defaults to 512.
+        overlap (int, optional): Overlap size for the DeepZoom pyramid. Defaults to 0.
+        suffix (str, optional): Suffix for the DeepZoom pyramid tiles. Defaults to ".jpeg".
+
+    Returns:
+        None
     """
-    Create a DeepZoom image pyramid from a JPEG image
-
-    Parameters
-    ----------
-    image_path : str
-        Path to the JPEG image file
-    tile_size : int (default=512)
-        Tile size for the DeepZoom pyramid
-    overlap : int (default=0)
-        Overlap size for the DeepZoom pyramid
-    suffix : str (default='jpeg')
-        Suffix for the DeepZoom pyramid tiles
-
-    Returns
-    -------
-    None
-
-    """
-
-    # Define the output path
     output_path = Path(output_path)
-
-    # Load the JPEG image
     image = pyvips.Image.new_from_file(image_path, access="sequential")
-
-    # check if the output path exists and create it if it does not
     output_path.mkdir(parents=True, exist_ok=True)
-
-    # append the pyramid name to the output path
     output_path = output_path / pyramid_name
-
-    # Save the image as a DeepZoom image pyramid
     image.dzsave(output_path, tile_size=tile_size, overlap=overlap, suffix=suffix)
 
 
 def make_meta_cell_image_coord(
-    technology,
-    path_transformation_matrix,
-    path_meta_cell_micron,
-    path_meta_cell_image,
-    image_scale
+    technology, path_transformation_matrix, path_meta_cell_micron, path_meta_cell_image, image_scale
 ):
+    """Applies an affine transformation to cell coordinates in microns and saves the transformed coordinates in pixels.
+
+    Args:
+        technology (str): The technology used to generate the data (e.g., "Xenium" or "MERSCOPE").
+        path_transformation_matrix (str): Path to the transformation matrix file.
+        path_meta_cell_micron (str): Path to the meta cell file with coordinates in microns.
+        path_meta_cell_image (str): Path to save the meta cell file with coordinates in pixels.
+        image_scale (float): Scaling factor to convert micron coordinates to pixel coordinates.
+
+    Returns:
+        None
     """
-    Apply an affine transformation to the cell coordinates in microns and save
-    the transformed coordinates in pixels
-
-    Parameters
-    ----------
-    technology : str
-        The technology used to generate the data, Xenium and MERSCOPE are supported.
-    path_transformation_matrix : str
-        Path to the transformation matrix file
-    path_meta_cell_micron : str
-        Path to the meta cell file with coordinates in microns
-    path_meta_cell_image : str
-        Path to save the meta cell file with coordinates in pixels
-
-    Returns
-    -------
-    None
-
-    Examples
-    --------
-    >>> make_meta_cell_image_coord(
-    ...     technology='Xenium',
-    ...     path_transformation_matrix='data/transformation_matrix.txt',
-    ...     path_meta_cell_micron='data/meta_cell_micron.csv',
-    ...     path_meta_cell_image='data/meta_cell_image.parquet'
-    ... )
-
-    """
-
-    transformation_matrix = pd.read_csv(
-        path_transformation_matrix, header=None, sep=" "
-    ).values
+    transformation_matrix = pd.read_csv(path_transformation_matrix, header=None, sep=" ").values
 
     if technology == "MERSCOPE":
         meta_cell = pd.read_csv(path_meta_cell_micron, usecols=["EntityID", "center_x", "center_y"])
         meta_cell = convert_long_id_to_short(meta_cell)
-        meta_cell["name"] =  meta_cell["cell_id"]
+        meta_cell["name"] = meta_cell["cell_id"]
         meta_cell = meta_cell.set_index('cell_id')
     elif technology == "Xenium":
         usecols = ["cell_id", "x_centroid", "y_centroid"]
@@ -255,97 +174,59 @@ def make_meta_cell_image_coord(
         meta_cell.columns = ["center_x", "center_y"]
         meta_cell["name"] = pd.Series(meta_cell.index, index=meta_cell.index)
 
-    # Adding a ones column to accommodate for affine transformation
     meta_cell["ones"] = 1
-
-    # Preparing the data for matrix multiplication
     points = meta_cell[["center_x", "center_y", "ones"]].values
-
-    # Applying the transformation matrix
     transformed_points = np.dot(transformation_matrix, points.T).T
 
-    # Updating the DataFrame with transformed coordinates
     meta_cell["center_x"] = transformed_points[:, 0]
     meta_cell["center_y"] = transformed_points[:, 1]
-
-    # Dropping the ones column as it's no longer needed
     meta_cell.drop(columns=["ones"], inplace=True)
 
     meta_cell["center_x"] = meta_cell["center_x"] / image_scale
     meta_cell["center_y"] = meta_cell["center_y"] / image_scale
 
-    meta_cell["geometry"] = meta_cell.apply(
-        lambda row: [row["center_x"], row["center_y"]], axis=1
-    )
+    meta_cell["geometry"] = meta_cell.apply(lambda row: [row["center_x"], row["center_y"]], axis=1)
 
     if technology == "MERSCOPE":
         meta_cell = meta_cell[["name", "geometry", "EntityID"]]
     else:
         meta_cell = meta_cell[["name", "geometry"]]
 
-
     meta_cell.to_parquet(path_meta_cell_image)
 
 
-
 def make_meta_gene(technology, path_cbg, path_output):
+    """Creates a DataFrame with genes and their assigned colors.
+
+    Args:
+        technology (str): The technology used to generate the data (e.g., "Xenium" or "MERSCOPE").
+        path_cbg (str): Path to the cell-by-gene matrix data.
+        path_output (str): Path to save the meta gene file.
+
+    Returns:
+        None
     """
-    Create a DataFrame with genes and their assigned colors
-
-    Parameters
-    ----------
-    technology : str
-        The technology used to generate the data, Xenium and MERSCOPE are supported.
-    path_cbg : str
-        Path to the cell-by-gene matrix data (the data format can vary based on technology)
-    path_output : str
-        Path to save the meta gene file
-
-    Returns
-    -------
-    None
-
-    Examples
-    --------
-    >>> make_meta_gene(
-    ...     technology='Xenium',
-    ...     path_cbg='data/',
-    ...     path_output='data/meta_gene.parquet'
-    ... )
-    """
-
     if technology == "MERSCOPE":
         cbg = pd.read_csv(path_cbg, index_col=0)
         genes = cbg.columns.tolist()
     elif technology == "Xenium":
-        # genes = pd.read_csv(path_cbg + 'features.tsv.gz', sep='\t', header=None)[1].values.tolist()
         cbg = read_cbg_mtx(path_cbg)
         genes = cbg.columns.tolist()
 
-    # Get all categorical color palettes from Matplotlib and flatten them into a single list of colors
     palettes = [plt.get_cmap(name).colors for name in plt.colormaps() if "tab" in name]
     flat_colors = [color for palette in palettes for color in palette]
-
-    # Convert RGB tuples to hex codes
     flat_colors_hex = [to_hex(color) for color in flat_colors]
 
-    # Use modular arithmetic to assign a color to each gene, white for genes with "Blank"
     colors = [
         flat_colors_hex[i % len(flat_colors_hex)] if "Blank" not in gene else "#FFFFFF"
         for i, gene in enumerate(genes)
     ]
 
-    # Create a DataFrame with genes and their assigned colors
     ser_color = pd.Series(colors, index=genes)
-
-    # calculate gene expression metadata
     meta_gene = calc_meta_gene_data(cbg)
     meta_gene['color'] = ser_color
 
-    # Identify sparse columns
     sparse_cols = [col for col in meta_gene.columns if pd.api.types.is_sparse(meta_gene[col])]
-
-    # Convert sparse columns to dense
     for col in sparse_cols:
         meta_gene[col] = meta_gene[col].sparse.to_dense()
 
@@ -353,40 +234,40 @@ def make_meta_gene(technology, path_cbg, path_output):
 
 
 def get_max_zoom_level(path_image_pyramid):
-    """
-    Returns the maximum zoom level based on the highest-numbered directory
-    in the specified path_image_pyramid.
+    """Returns the maximum zoom level based on the highest-numbered directory in the specified path.
 
-    Parameters:
-        path_image_pyramid (str): The path to the directory containing zoom level directories.
+    Args:
+        path_image_pyramid (str): Path to the directory containing zoom level directories.
 
     Returns:
-        max_pyramid_zoom (int): The maximum zoom level.
+        int: The maximum zoom level.
     """
-    # List all entries in the path_image_pyramid that are directories and can be converted to integers
     zoom_levels = [
         entry
         for entry in os.listdir(path_image_pyramid)
         if os.path.isdir(os.path.join(path_image_pyramid, entry)) and entry.isdigit()
     ]
-
-    # Convert to integer and find the maximum value
     max_pyramid_zoom = max(map(int, zoom_levels)) if zoom_levels else None
-
     return max_pyramid_zoom
 
 
 def save_landscape_parameters(
     technology, path_landscape_files, image_name="dapi_files", tile_size=1000, image_info={}, image_format='.webp'
 ):
-    """
-    Save the landscape parameters to a JSON file.
-    """
+    """Saves the landscape parameters to a JSON file.
 
+    Args:
+        technology (str): The technology used to generate the data.
+        path_landscape_files (str): Path to the directory where landscape files are stored.
+        image_name (str, optional): Name of the image directory. Defaults to "dapi_files".
+        tile_size (int, optional): Tile size for the image pyramid. Defaults to 1000.
+        image_info (dict, optional): Additional image metadata. Defaults to {}.
+        image_format (str, optional): Format of the image files. Defaults to ".webp".
+
+    Returns:
+        None
+    """
     path_image_pyramid = f"{path_landscape_files}/pyramid_images/{image_name}"
-
-    print(path_image_pyramid)
-
     max_pyramid_zoom = get_max_zoom_level(path_image_pyramid)
 
     landscape_parameters = {
@@ -398,24 +279,24 @@ def save_landscape_parameters(
     }
 
     path_landscape_parameters = f"{path_landscape_files}/landscape_parameters.json"
-
     with open(path_landscape_parameters, "w") as file:
         json.dump(landscape_parameters, file, indent=4)
 
 
 def to_geometry(coord_list):
+    """Converts a coordinates list to a Shapely geometry object (Point or Polygon).
+
+    Args:
+        coord_list (list or Point or Polygon): Input coordinates or geometry object.
+
+    Returns:
+        Point or Polygon: Shapely geometry object.
     """
-    Convert coordinates list to shapely geometry used in GeoDataFrame
-    """
-    # If already a Point or Polygon, return it directly
     if isinstance(coord_list, (Point, Polygon)):
         return coord_list
-    
-    # If it’s a single coordinate pair, create a Point
-    if isinstance(coord_list[0], (int, float)):  # Single coordinate pair
+    if isinstance(coord_list[0], (int, float)):
         return Point(coord_list)
-    
-    # If it's a list of coordinate pairs, create a Polygon
     return Polygon(coord_list)
+
 
 __all__ = ["landscape", "trx_tile", "boundary_tile"]

b54d9f665022cec883336a5594dec1a13760422c
Huan Wang <huanlity@gmail.com>
2025-03-27 12:35:45 -0400

    make cell_exp_array back-compatible

--- js/global_variables/cell_exp_array.js
+++ js/global_variables/cell_exp_array.js
@@ -1,7 +1,14 @@
 import { get_arrow_table } from '../read_parquet/get_arrow_table'
 import { options } from '../global_variables/fetch_options.js'
 
-export const update_cell_exp_array = async (cats, genes, base_url, inst_gene, version) => {
+function processExpression(exp_value, max_exp) {
+    const log_exp_value = Math.log1p(exp_value);
+    const log_max_exp = Math.log1p(max_exp);
+    return (log_exp_value / log_max_exp) * 255;
+}
+
+
+export const update_cell_exp_array = async (cats, genes, base_url, inst_gene, version, vector_name_integer) => {
 
     let file_path;
     if (version === 'default'){
@@ -16,22 +23,29 @@ export const update_cell_exp_array = async (cats, genes, base_url, inst_gene, ve
     let cell_exp = exp_table.getChild(inst_gene).toArray()
 
     const new_exp_array = new Array(cats.cell_names_array.length).fill(0)
+
+    console.log('vector_name_integer:',vector_name_integer)
     
     cell_names.forEach((name, i) => {
-        if (name in cats.nameMapping_inv) {
-            const exp_value = Number(cell_exp[i]);
-            const max_exp = Number(genes.meta_gene[inst_gene].max);
+        name = String(name);
+        const exp_value = Number(cell_exp[i]);
+        const max_exp = Number(genes.meta_gene[inst_gene].max);
 
-            // Apply logarithmic transformation
-            const log_exp_value = Math.log1p(exp_value); // log1p(x) = log(1 + x)
-
-            // Scale to 0-255 range using the log of the max expression value
-            const log_max_exp = Math.log1p(max_exp); // log1p(x) = log(1 + x)
-            new_exp_array[name] = (log_exp_value / log_max_exp) * 255;
+        if (!vector_name_integer) {
+            if (cats.cell_name_to_index_map.has(name)) {
+                const index = cats.cell_name_to_index_map.get(name);
+                new_exp_array[index] = processExpression(exp_value, max_exp);
+            } else {
+                console.log('Cell name not found in cell_name_to_index_map');
+            }
         } else {
-            console.log('Cell name not found in cats.nameMapping_inv');
+            if (name in cats.nameMapping_inv) {
+                new_exp_array[name] = processExpression(exp_value, max_exp);
+            } else {
+                console.log('Cell name not found in cats.nameMapping_inv');
+            }
         }
     });
 
-    cats.cell_exp_array = new_exp_array
+    cats.cell_exp_array = new_exp_array;
 }

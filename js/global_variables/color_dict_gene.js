49daca70d18a60f30a04b46dc870c8ce66193648
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-03-27 16:38:22 -0400

    remove console

--- js/global_variables/color_dict_gene.js
+++ js/global_variables/color_dict_gene.js
@@ -6,15 +6,11 @@ export const set_color_dict_gene = async (genes, base_url, seg_version='default'
 
     let meta_gene_url
 
-    console.log('seg_version', seg_version)
-
     if (seg_version === 'default'){
         meta_gene_url = base_url + '/meta_gene.parquet';
     } else {
         meta_gene_url = base_url + '/meta_gene_' + seg_version + '.parquet';
     }
-
-    console.log('meta_gene_url', meta_gene_url)
 
     let tmp_meta_gene = await get_arrow_table(meta_gene_url, options.fetch)
 
--- js/global_variables/meta_gene.js
+++ js/global_variables/meta_gene.js
@@ -5,15 +5,11 @@ export const set_meta_gene = async (genes, base_url, seg_version='default') => {
 
     let meta_gene_url
 
-    console.log('seg_version', seg_version)
-
     if (seg_version === 'default'){
         meta_gene_url = base_url + '/meta_gene.parquet';
     } else {
         meta_gene_url = base_url + '/meta_gene_' + seg_version + '.parquet';
     }
-
-    console.log('meta_gene_url', meta_gene_url)
 
     let meta_gene_table = await get_arrow_table(meta_gene_url, options.fetch)
 

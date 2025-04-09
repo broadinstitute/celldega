81d48236c9bdfd0d5d798a56332910f58e75e583
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-04-09 07:17:25 -0400

    clean create_get_tile_data

--- js/deck-gl/create_get_tile_data.js
+++ js/deck-gl/create_get_tile_data.js
@@ -1,24 +1,3 @@
-// import { load } from '@loaders.gl/core';
-
-// export const create_get_tile_data = (base_url, image_name, image_format, max_image_zoom, options, aws) => {
-
-//     console.log('create_get_tile_data: aws', aws)
-
-//     return ({ index }) => {
-//         const { x, y, z } = index;
-//         const full_url = `${base_url}/pyramid_images/${image_name}_files/${max_image_zoom + z}/${x}_${y}${image_format}`;
-
-//         return load(full_url, options).then(data => {
-//             console.log('data', data)
-//             return data;
-//         }).catch(error => {
-//             console.error('Failed to load tile:', error);
-//             return null;
-//         });
-//     };
-// };
-
-
 import { load } from '@loaders.gl/core';
 import { ImageLoader } from '@loaders.gl/images';
 
@@ -52,38 +31,3 @@ export const create_get_tile_data = (
         }
     };
 };
-
-
-
-
-// import { load } from '@loaders.gl/core';
-// import { ImageLoader } from '@loaders.gl/images';
-
-// export const create_get_tile_data = (base_url, image_name, image_format, max_image_zoom, options, aws = null) => {
-//     return async ({ index }) => {
-//         const { x, y, z } = index;
-//         const full_url = `${base_url}/pyramid_images/${image_name}_files/${max_image_zoom + z}/${x}_${y}${image_format}`;
-
-//         try {
-//             if (aws !== null) {
-//                 const response = await aws.fetch(full_url);
-//                 const arrayBuffer = await response.arrayBuffer();
-
-//                 return await load(arrayBuffer, {
-//                     loader: ImageLoader,
-//                     mimeType: 'image/webp',
-//                     ...options
-//                 });
-//             } else {
-//                 return await load(full_url, {
-//                     loader: ImageLoader,
-//                     ...options
-//                 });
-//             }
-
-//         } catch (error) {
-//             console.error('Failed to load tile:', error);
-//             return null;
-//         }
-//     };
-// };

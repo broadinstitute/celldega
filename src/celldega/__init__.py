ed8b80316cc33397bacbd2cc579551c09aaad529
Amin Ben Saad <41109680+aminbensaad@users.noreply.github.com>
2025-05-26 19:38:09 -0400

    optim(formatting): updated celldega init

--- src/celldega/__init__.py
+++ src/celldega/__init__.py
@@ -1,14 +1,12 @@
 import importlib.metadata
 
-from celldega.viz import Landscape, Matrix
-from celldega.pre import landscape
-from celldega.qc import qc_segmentation
-from celldega.nbhd import alpha_shape
-
-from celldega.clust import Network
-
 # temporary fix for libpysal warning
 import warnings
+
+from celldega.pre import landscape
+from celldega.viz import Landscape, Matrix
+
+
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 try:
@@ -16,4 +14,4 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
-__all__ = ["Landscape", "landscape", "Matrix"]
+__all__ = ["Landscape", "Matrix", "landscape"]

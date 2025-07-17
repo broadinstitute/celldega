b399a477d4089f159b514892609ed31652a90f2f
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-07-17 16:16:55 -0400

    lint

--- js/viz/landscape_ist.js
+++ js/viz/landscape_ist.js
@@ -12,7 +12,6 @@ import {
 import { set_views } from '../deck-gl/core/views';
 import {
   ini_background_layer,
-  toggle_background_layer_visibility,
 } from '../deck-gl/layers/background_layer';
 import {
   ini_cell_layer,
@@ -24,7 +23,6 @@ import {
 //   set_edit_layer_on_edit,
 // } from '../deck-gl/layers/edit_layer';
 import {
-  make_image_layers,
   toggle_visibility_image_layers,
 } from '../deck-gl/layers/image_layers';
 // import {
@@ -58,9 +56,9 @@ import { set_landscape_parameters } from '../global_variables/landscape_paramete
 import { set_cluster_metadata } from '../global_variables/meta_cluster';
 import { set_meta_gene } from '../global_variables/meta_gene';
 import { update_selected_genes } from '../global_variables/selected_genes';
-import { get_img_layer_visible } from '../ui/text_buttons';
 import { create_obs_store } from '../obs_store/obs_store';
 import { set_image_layer_sliders } from '../ui/sliders';
+import { get_img_layer_visible } from '../ui/text_buttons';
 import { make_ist_ui_container } from '../ui/ui_containers';
 import { update_cell_clusters } from '../widget_interactions/update_cell_clusters';
 import { update_ist_landscape_from_cgm } from '../widget_interactions/update_ist_landscape_from_cgm';
--- js/widget.js
+++ js/widget.js
@@ -39,8 +39,6 @@ const render_landscape_ist = async ({ model, el }) => {
   }
 
   const umapBytes = model.get('umap_parquet');
-  if (umapBytes && umapBytes.byteLength > 0) {
-  }
 
   const landscape_state = model.get('landscape_state');
   const segmentation = model.get('segmentation');

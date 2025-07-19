eff0d08d150fda15749fb6ce771b47bda39634b3
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-07-18 23:45:37 -0400

    debug parquet reader

--- js/read_parquet/get_polygon_data.js
+++ js/read_parquet/get_polygon_data.js
@@ -4,15 +4,31 @@ export const get_polygon_data = (arrowTable) => {
   const geometryColumn = arrowTable.getChildAt(0);
   const typeId = geometryColumn.data[0].type.typeId;
 
-  if (typeId === Type.List) {
+  console.log('get_polygon_data!!', typeId, geometryColumn);
+
+  // if (typeId === Type.List) {
+
+    console.log('here')
     const polygonIndices = geometryColumn.data[0].valueOffsets;
+    console.log('post polygonIndices:', polygonIndices);
+    console.log('geometryColumn.getChildAt(0)', geometryColumn.getChildAt(0));
     const ringIndices = geometryColumn.getChildAt(0).data[0].valueOffsets;
+    console.log('post ringIndices:', ringIndices);
     const flatCoordinateVector = geometryColumn
       .getChildAt(0)
       .getChildAt(0)
       .getChildAt(0);
     const flatCoordinateArray = flatCoordinateVector.data[0].values;
     const resolvedIndices = new Int32Array(polygonIndices.length);
+
+    console.log(
+      'polygonIndices:',
+      polygonIndices,
+      'ringIndices:',
+      ringIndices,
+      'flatCoordinateArray:',
+      flatCoordinateArray
+    );
 
     for (let i = 0; i < resolvedIndices.length; ++i) {
       // Perform the lookup into the ringIndices array using the polygonIndices array
@@ -29,34 +45,44 @@ export const get_polygon_data = (arrowTable) => {
         getPolygon: { value: flatCoordinateArray, size: 2 },
       },
     };
+
     return data;
-  } else if (typeId === Type.Utf8) {
-    const jsonValues = geometryColumn.toArray();
-    const polygons = Array.from(jsonValues, (d) => JSON.parse(d));
 
-    const start = new Int32Array(polygons.length + 1);
-    const coords = [];
-    let offset = 0;
+  // } else if (typeId === Type.Utf8) {
 
-    for (let i = 0; i < polygons.length; i++) {
-      start[i] = offset;
-      const rings = polygons[i];
-      if (rings.length > 0) {
-        const firstRing = rings[0];
-        for (const [x, y] of firstRing) {
-          coords.push(x, y);
-          offset += 1;
-        }
-      }
-    }
-    start[polygons.length] = offset;
+  //   console.log('get_polygon_data: Utf8 type detected');
 
-    return {
-      length: polygons.length,
-      startIndices: start,
-      attributes: { getPolygon: { value: new Float64Array(coords), size: 2 } },
-    };
-  } else {
-    return null;
-  }
+  //   const jsonValues = geometryColumn.toArray();
+  //   const polygons = Array.from(jsonValues, (d) => JSON.parse(d));
+
+  //   const start = new Int32Array(polygons.length + 1);
+  //   const coords = [];
+  //   let offset = 0;
+
+  //   for (let i = 0; i < polygons.length; i++) {
+  //     start[i] = offset;
+  //     const rings = polygons[i];
+  //     if (rings.length > 0) {
+  //       const firstRing = rings[0];
+  //       for (const [x, y] of firstRing) {
+  //         coords.push(x, y);
+  //         offset += 1;
+  //       }
+  //     }
+  //   }
+  //   start[polygons.length] = offset;
+
+  //   return {
+  //     length: polygons.length,
+  //     startIndices: start,
+  //     attributes: { getPolygon: { value: new Float64Array(coords), size: 2 } },
+  //   };
+  // } else {
+  //   console.error(
+  //     'Unsupported geometry type in get_polygon_data:',
+  //     typeId,
+  //     geometryColumn
+  //   );
+  //   return null;
+  // }
 };

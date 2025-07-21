9441fddf4542ec4b1c2198cd69430e165f8f419c
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-07-21 17:12:53 -0400

    toggling with NBHD button

--- js/viz/landscape_ist.js
+++ js/viz/landscape_ist.js
@@ -128,7 +128,7 @@ export const landscape_ist = async (
 
   viz_state.obs_store.viz_nbhd_layer.subscribe((visible) => {
 
-    console.log('here')
+    console.log('viz_nbhd_layer', visible)
     if (visible) {
       viz_state.obs_store.viz_image_layers.set(false);
       viz_state.obs_store.viz_background_layer.set(false);

84c5b9ff5ee9f64b5d6ca2dabf3904b7766af30d
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-07-21 23:25:46 -0400

    removed console

--- js/viz/landscape_ist.js
+++ js/viz/landscape_ist.js
@@ -390,8 +390,6 @@ export const landscape_ist = async (
 
   viz_state.obs_store.viz_nbhd_layer.subscribe((visible) => {
 
-    console.log('viz_nbhd_layer changed to', visible);
-
     if (visible) {
       viz_state.obs_store.viz_image_layers.set(false);
       viz_state.obs_store.viz_background_layer.set(false);

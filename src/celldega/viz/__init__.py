9aed2c652acff7cbededefe4c5e9cd14ec6cddb6
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-07-31 18:21:44 -0400

    fix widget linking

--- src/celldega/viz/__init__.py
+++ src/celldega/viz/__init__.py
@@ -24,7 +24,17 @@ def landscape_clustergram(landscape, mat, width="600px", height="700px"):
     Example:
     See example [Landscape-Matrix_Xenium](../../../examples/brief_notebooks/Landscape-Matrix_Xenium) notebook
     """
-    # Use `jslink` to directly link `click_info` from `mat` to `trigger_value` in `landscape_ist`
+    # Ensure widgets have an active comm before linking. `AnyWidget` instances
+    # do not instantiate a dummy Comm until ``open()`` is called which can
+    # cause ``jslink`` to fail with ``AttributeError`` when accessing
+    # ``model_id``.
+    if getattr(mat, "comm", None) is None:
+        mat.open()
+    if getattr(landscape, "comm", None) is None:
+        landscape.open()
+
+    # Use `jslink` to directly link `click_info` from `mat` to
+    # `update_trigger` in `landscape`
     jslink((mat, "click_info"), (landscape, "update_trigger"))
 
     # Set layouts for the widgets

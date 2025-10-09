4b18df13c5c463065f70f6d0169ccea81a867ffa
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-10-09 16:44:21 -0400

    removed unnecessary import

--- src/celldega/pre/__init__.py
+++ src/celldega/pre/__init__.py
@@ -32,7 +32,6 @@ from .boundary_tile import (
 from .image_info import get_image_info
 from .landscape import calc_meta_gene_data, read_cbg_mtx, save_cbg_gene_parquets
 from .run_pre_processing import main
-from .sbg_tile import write_pseudotranscripts_from_sbg
 from .trx_tile import make_trx_tiles
 
 

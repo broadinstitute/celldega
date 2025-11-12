c89ba36199959fdc94c42fc1a2cc48833f2d96aa
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-11-12 12:17:46 -0500

    Add animated dendrogram double-click zoom focus

--- js/deck-gl/matrix/dendro_layers.js
+++ js/deck-gl/matrix/dendro_layers.js
@@ -1,12 +1,125 @@
-import { PolygonLayer } from 'deck.gl';
+import { LinearInterpolator, PolygonLayer } from 'deck.gl';
 
 import { sync_selected_genes } from '../../global_variables/selected_genes';
 
+import { curate_pan_x, curate_pan_y } from './curate_pan';
 import { get_mat_layers_list } from './matrix_layers';
+import { redefine_global_view_state } from './redefine_global_view_state';
+import { update_zoom_data } from './zoom';
 
 const DENDRO_AXES = ['row', 'col'];
 const DEFAULT_FILL_COLOR = [0, 0, 0, 90];
 const FOCUSED_FILL_COLOR = [0, 0, 0, 180];
+const DOUBLE_CLICK_DELAY = 250;
+const DENDRO_FOCUS_TRANSITION = 650;
+const MAX_FOCUS_ZOOM = 7;
+
+const easeOutCubic = (t) => 1 - Math.pow(1 - t, 3);
+
+const ensureClickTracking = (viz_state) => {
+  if (!viz_state.dendro.clicks) {
+    viz_state.dendro.clicks = { row: 0, col: 0 };
+  }
+
+  if (!viz_state.dendro.clickTimeouts) {
+    viz_state.dendro.clickTimeouts = { row: null, col: null };
+  }
+};
+
+const applyViewStateTransition = (viewState) => {
+  const transitionProps = () => ({
+    transitionDuration: DENDRO_FOCUS_TRANSITION,
+    transitionEasing: easeOutCubic,
+    transitionInterpolator: new LinearInterpolator(['target', 'zoom']),
+  });
+
+  return Object.fromEntries(
+    Object.entries(viewState).map(([key, value]) => [
+      key,
+      { ...value, ...transitionProps() },
+    ])
+  );
+};
+
+const computeClusterZoom = (matrixSpan, clusterSpan, minUnit) => {
+  const paddedSpan = Math.min(
+    matrixSpan,
+    Math.max(clusterSpan * 1.2, minUnit * 3)
+  );
+
+  const zoom = Math.log2(matrixSpan / Math.max(paddedSpan, 1e-6));
+
+  return Math.min(Math.max(zoom, 0), MAX_FOCUS_ZOOM);
+};
+
+const animate_focus_to_cluster = (
+  deck_mat,
+  layers_mat,
+  viz_state,
+  axis,
+  polygonProps
+) => {
+  if (!polygonProps) {
+    return;
+  }
+
+  const isRowAxis = axis === 'row';
+  const matrixSpan = isRowAxis
+    ? viz_state.viz.mat_height
+    : viz_state.viz.mat_width;
+  const minUnit = isRowAxis
+    ? viz_state.viz.row_offset
+    : viz_state.viz.col_offset;
+
+  const currentZoomX = viz_state.zoom.zoom_data.matrix.zoom_x;
+  const currentZoomY = viz_state.zoom.zoom_data.matrix.zoom_y;
+
+  const clusterSpan = Math.max(
+    Math.abs((polygonProps.pos_bot ?? 0) - (polygonProps.pos_top ?? 0)),
+    minUnit
+  );
+
+  const targetZoom = computeClusterZoom(matrixSpan, clusterSpan, minUnit);
+
+  const zoom_curated = [
+    isRowAxis ? currentZoomX : targetZoom,
+    isRowAxis ? targetZoom : currentZoomY,
+  ];
+
+  const clusterCenter =
+    ((polygonProps.pos_top ?? 0) + (polygonProps.pos_bot ?? 0)) / 2;
+
+  const existingPan = viz_state.zoom.zoom_data.matrix;
+
+  const desiredPan = {
+    x: isRowAxis ? existingPan.pan_x : clusterCenter,
+    y: isRowAxis ? clusterCenter : existingPan.pan_y,
+  };
+
+  const pan_curated = [
+    curate_pan_x(desiredPan.x, zoom_curated[0], viz_state),
+    curate_pan_y(desiredPan.y, zoom_curated[1], viz_state),
+  ];
+
+  const viewId = isRowAxis ? 'dendro_rows' : 'dendro_cols';
+
+  const globalViewState = redefine_global_view_state(
+    viz_state,
+    viewId,
+    zoom_curated,
+    pan_curated
+  );
+
+  update_zoom_data(viz_state, viewId, zoom_curated, pan_curated);
+
+  viz_state.zoom.zoom_data.total_zoom.x = zoom_curated[0];
+  viz_state.zoom.zoom_data.total_zoom.y = zoom_curated[1];
+
+  deck_mat.setProps({
+    viewState: applyViewStateTransition(globalViewState),
+    layers: get_mat_layers_list(layers_mat),
+  });
+};
 
 const getCurrentFocus = (viz_state) => {
   const storeFocus =
@@ -151,6 +264,10 @@ const focus_dendro_polygon = (
 };
 
 const dendro_layer_onclick = (event, deck_mat, layers_mat, viz_state, axis) => {
+  ensureClickTracking(viz_state);
+
+  viz_state.dendro.clicks[axis] += 1;
+
   viz_state.click.type = `${axis}_dendro`;
 
   viz_state.click.value = {
@@ -179,6 +296,28 @@ const dendro_layer_onclick = (event, deck_mat, layers_mat, viz_state, axis) => {
   if (typeof viz_state.custom_callbacks[`${axis}_dendro`] === 'function') {
     viz_state.custom_callbacks[`${axis}_dendro`](
       event.object.properties.all_names
+    );
+  }
+
+  if (viz_state.dendro.clicks[axis] === 1) {
+    viz_state.dendro.clickTimeouts[axis] = setTimeout(() => {
+      viz_state.dendro.clicks[axis] = 0;
+      viz_state.dendro.clickTimeouts[axis] = null;
+    }, DOUBLE_CLICK_DELAY);
+  } else if (viz_state.dendro.clicks[axis] === 2) {
+    if (viz_state.dendro.clickTimeouts[axis]) {
+      clearTimeout(viz_state.dendro.clickTimeouts[axis]);
+      viz_state.dendro.clickTimeouts[axis] = null;
+    }
+
+    viz_state.dendro.clicks[axis] = 0;
+
+    animate_focus_to_cluster(
+      deck_mat,
+      layers_mat,
+      viz_state,
+      axis,
+      event.object.properties
     );
   }
 };

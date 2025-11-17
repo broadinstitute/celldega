debea5f4e9b74e93d79a1db639bcc80a7289ccb8
Nicolas Fernandez <cornhundred@users.noreply.github.com>
2025-11-16 22:30:59 -0500

    revert cat_layers.js

--- js/deck-gl/matrix/cat_layers.js
+++ js/deck-gl/matrix/cat_layers.js
@@ -1,6 +1,15 @@
+import * as d3 from 'd3';
+
 import { CustomMatrixLayer } from './custom_matrix_layer';
 
 export const ini_row_cat_layer = (viz_state) => {
+  const transitions = {
+    getPosition: {
+      duration: viz_state.animate.duration,
+      easing: d3.easeCubic,
+    },
+  };
+
   const row_cat_layer = new CustomMatrixLayer({
     id: 'row-layer',
     data: viz_state.cats.row_cat_data,
@@ -18,6 +27,7 @@ export const ini_row_cat_layer = (viz_state) => {
     },
     getFillColor: (d) => d.color,
     pickable: true,
+    transitions,
     opacity: 0.8,
     tile_width: (viz_state.viz.row_cat_width / 2) * 0.9,
     tile_height: (viz_state.viz.mat_height / viz_state.mat.num_rows) * 0.5,
@@ -27,6 +37,13 @@ export const ini_row_cat_layer = (viz_state) => {
 };
 
 export const ini_col_cat_layer = (viz_state) => {
+  const transitions = {
+    getPosition: {
+      duration: viz_state.animate.duration,
+      easing: d3.easeCubic,
+    },
+  };
+
   const col_cat_layer = new CustomMatrixLayer({
     id: 'col-layer',
     data: viz_state.cats.col_cat_data,
@@ -44,6 +61,7 @@ export const ini_col_cat_layer = (viz_state) => {
     },
     getFillColor: (d) => d.color,
     pickable: true,
+    transitions,
     opacity: 0.8,
     tile_width: (viz_state.viz.mat_width / viz_state.mat.num_cols) * 0.5,
     tile_height: viz_state.viz.col_cat_height / 2,
